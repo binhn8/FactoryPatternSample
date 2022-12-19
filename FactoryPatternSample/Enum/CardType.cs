8e9fb6a3cc787cefd8cdd35c2fe9d242447b009e
Binh Nguyen <binh.nguyen@printerpix.co.uk>
2022-12-19 12:32:24 +0000

    Tidy

--- FactoryPatternSample/Enum/CardType.cs
+++ FactoryPatternSample/Enum/CardType.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace FactoryPatternSample
+﻿namespace FactoryPatternSample
 {
 	public enum CardType {
 		Standard,
